2bb92500dfe19b201c5245beaf00c09f7dbabe11
Viseth Taylor <taylorv@roverkids.org>
2026-04-20 14:02:03 -0400

    challenge stuff

---
+++ PlaneProgramming/PlaneChallenge/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 15.0f;
+    public float rotationSpeed = 100.0f;
+    public float verticalInput = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
